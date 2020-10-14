ca7221dab24ac7d5fdc14fef03d1e86871d6f865
slyroman <72763566+slyroman@users.noreply.github.com>
2020-10-14 22:36:24 +0500

    last

--- src/main/scala/JsonReader.scala
+++ src/main/scala/JsonReader.scala
@@ -1,6 +1,6 @@
 
 object JsonReader extends  App {
-//  println(args.mkString(", "))
+  //println(args.toString)
   import org.apache.log4j.{Level, Logger}
   Logger.getLogger("org").setLevel(Level.OFF)
 
@@ -12,15 +12,26 @@ object JsonReader extends  App {
     .config("spark.testing.memory", "471859200")
     .getOrCreate()
 
+  case class Wine(
+
+                   id: Int,
+                   country: String,
+                   points: Int,
+                   title: String,
+                   variety: String,
+                   winery: String
+
+                 )
 
   def sc = spark.sparkContext
-  val lines = sc.textFile("json_src/winemag-data-130k-v2.json")
+  val lines = sc.textFile(args(0).toString)
   import org.json4s._
   import org.json4s.jackson.JsonMethods._
 
   implicit val formats = DefaultFormats
   lines.map{ row =>
     val json_row = parse(row)
+    //.extract[Wine]
     (compact(json_row \ "id"), compact(json_row \ "country"), compact(json_row \ "points"), compact(json_row \ "title"),
       compact(json_row \ "variety"), compact(json_row \ "winery"))
       .toString
